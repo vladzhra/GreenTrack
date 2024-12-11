aae1f914c9384a63bbe74d6d6187bb7dac040e2b
Zaharia Vlad <vlad.zaharia@epitech.eu>
2024-12-11 12:11:59 +0100

    Add MainScreen

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { TransitionPresets } from "@react-navigation/stack";
+import { createStackNavigator } from "@react-navigation/stack";
+import "react-native-gesture-handler";
+
+import MainScreen from "./src/MainScreen";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Main"
+        screenOptions={{
+          headerShown: false,
+          ...TransitionPresets.ModalFadeTransition,
+        }}
+      >
+        <Stack.Screen
+          name="Main"
+          component={MainScreen}
+          options={{ title: "Accueil" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
